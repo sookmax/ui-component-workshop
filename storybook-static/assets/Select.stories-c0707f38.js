c44307b6c737c463391665cc56c0d43c3319d3e0
sook <sukkyu.chung@gmail.com>
2023-07-07 23:42:58 +0900

    build

---
+++ storybook-static/assets/Select.stories-c0707f38.js
@@ -0,0 +1,40 @@
+import{j as r}from"./jsx-runtime-3cfe299f.js";import{S as m}from"./Select-b1cc77bf.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index-07d1f67e.js";import"./index-8c945b47.js";import"./index-09856750.js";import"./index-01ce9cbc.js";import"./index-18f3de2c.js";import"./index-3bb2a33e.js";import"./cn-ad0adaed.js";import"./react-icons.esm-c66481c4.js";const j={component:m,parameters:{layout:"centered"},tags:["autodocs"],decorators:[u=>r.jsx("div",{className:"w-[400px]",children:r.jsx(u,{})})],args:{label:"Fruits",placeholder:"Select a fruit..."}},a={args:{items:[{value:"apple",label:"Apple"},{value:"orange",label:"Orange"},{value:"grape",label:"Grape"},{value:"strawberry",label:"Strawberry"},{value:"banana",label:"Banana"},{value:"cherry",label:"Cherry"}]}},e={args:{items:[{value:"apple"},{value:"orange"},{value:"grape"},{value:"strawberry"},{value:"banana"},{value:"cherry"}]}};var l,n,t;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      value: "apple",
+      label: "Apple"
+    }, {
+      value: "orange",
+      label: "Orange"
+    }, {
+      value: "grape",
+      label: "Grape"
+    }, {
+      value: "strawberry",
+      label: "Strawberry"
+    }, {
+      value: "banana",
+      label: "Banana"
+    }, {
+      value: "cherry",
+      label: "Cherry"
+    }]
+  }
+}`,...(t=(n=a.parameters)==null?void 0:n.docs)==null?void 0:t.source}}};var o,s,p;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      value: "apple"
+    }, {
+      value: "orange"
+    }, {
+      value: "grape"
+    }, {
+      value: "strawberry"
+    }, {
+      value: "banana"
+    }, {
+      value: "cherry"
+    }]
+  }
+}`,...(p=(s=e.parameters)==null?void 0:s.docs)==null?void 0:p.source}}};const N=["Default","OnlyValueNoLabel"];export{a as Default,e as OnlyValueNoLabel,N as __namedExportsOrder,j as default};
+//# sourceMappingURL=Select.stories-c0707f38.js.map
--- storybook-static/assets/WithTooltip-FBT32F6Q-21cc6d90.js
+++
@@ -1,2 +0,0 @@
-import{W as f,W as u,a as x}from"./index-9b5c2e51.js";import"./iframe-83cc9432.js";import"../sb-preview/runtime.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./index.modern-e8a57965.js";import"./index-07d1f67e.js";import"./index-d37d4223.js";import"./extends-98964cd2.js";import"./index-f8236e9a.js";import"./index-356e4a49.js";export{f as WithToolTipState,u as WithTooltip,x as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-21cc6d90.js.map
