31ea27d1b3a2853127e77869060784591a402457
sook <sukkyu.chung@gmail.com>
2023-07-11 23:26:46 +0900

    build

--- storybook-static/assets/InputWithLabel.stories-8023abd1.js
+++
@@ -1,8 +0,0 @@
-import{j as a}from"./jsx-runtime-3cfe299f.js";import{I as n}from"./InputWithLabel-de09789f.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./cn-ad0adaed.js";import"./Input-785f32be.js";import"./WithLabel-f01f0bd9.js";import"./extends-98964cd2.js";import"./index-68f9b7fa.js";import"./index-07d1f67e.js";const E={component:n,parameters:{layout:"centered"},tags:["autodocs"],decorators:[d=>a.jsx("div",{className:"w-[350px]",children:a.jsx(d,{})})],args:{input:{id:"email-input",placeholder:"type a valid email address."},label:{htmlFor:"email-input",children:"Email"}}},r={},e={args:{input:{disabled:!0}}};var t,s,o;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(o=(s=r.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var i,p,m;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
-  args: {
-    input: {
-      disabled: true
-    }
-  }
-}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const v=["Default","Disabled"];export{r as Default,e as Disabled,v as __namedExportsOrder,E as default};
-//# sourceMappingURL=InputWithLabel.stories-8023abd1.js.map
---
+++ storybook-static/assets/SelectWithLabel.stories-d5824a93.js
@@ -0,0 +1,42 @@
+import{j as a}from"./jsx-runtime-3cfe299f.js";import{S as m}from"./SelectWithLabel-107b003e.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./cn-ad0adaed.js";import"./Select-15286fa3.js";import"./extends-98964cd2.js";import"./index-07d1f67e.js";import"./index-5ab62eb6.js";import"./index-09856750.js";import"./index-f43e56be.js";import"./index-68f9b7fa.js";import"./index-92ec52e4.js";import"./index-9a763fba.js";import"./index-6d37f43d.js";import"./react-icons.esm-44c0a4a4.js";import"./WithLabel-895a6a98.js";const E={component:m,parameters:{layout:"centered"},tags:["autodocs"],decorators:[u=>a.jsx("div",{className:"w-[400px]",children:a.jsx(u,{})})]},e={args:{select:{label:"Fruits",placeholder:"Select a fruit...",items:[{value:"apple",label:"Apple"},{value:"orange",label:"Orange"},{value:"grape",label:"Grape"},{value:"strawberry",label:"Strawberry"},{value:"banana",label:"Banana"},{value:"cherry",label:"Cherry"}],triggerId:"fruit-select"},label:{children:"Your favorite fruit",htmlFor:"fruit-select"}}};var l,t;const r={args:{label:(l=e.args)==null?void 0:l.label,select:{items:[],disabled:!0,...(t=e.args)==null?void 0:t.select}}};var n,s,o;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    select: {
+      label: "Fruits",
+      placeholder: "Select a fruit...",
+      items: [{
+        value: "apple",
+        label: "Apple"
+      }, {
+        value: "orange",
+        label: "Orange"
+      }, {
+        value: "grape",
+        label: "Grape"
+      }, {
+        value: "strawberry",
+        label: "Strawberry"
+      }, {
+        value: "banana",
+        label: "Banana"
+      }, {
+        value: "cherry",
+        label: "Cherry"
+      }],
+      triggerId: "fruit-select"
+    },
+    label: {
+      children: "Your favorite fruit",
+      htmlFor: "fruit-select"
+    }
+  }
+}`,...(o=(s=e.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var i,p,c;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: Default.args?.label,
+    select: {
+      items: [],
+      disabled: true,
+      ...Default.args?.select
+    }
+  }
+}`,...(c=(p=r.parameters)==null?void 0:p.docs)==null?void 0:c.source}}};const G=["Default","Disabled"];export{e as Default,r as Disabled,G as __namedExportsOrder,E as default};
+//# sourceMappingURL=SelectWithLabel.stories-d5824a93.js.map
