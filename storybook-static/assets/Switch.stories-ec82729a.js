6e98a9f1714d9b1a8edadf506c987071781cb2c7
sook <sukkyu.chung@gmail.com>
2023-07-14 23:25:06 +0900

    build

---
+++ storybook-static/assets/Switch.stories-ec82729a.js
@@ -0,0 +1,15 @@
+import{S as D}from"./Switch-a4d89b8d.js";import"./jsx-runtime-3cfe299f.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index-027e524b.js";import"./index-ad15057c.js";import"./index-5b9e656f.js";import"./index-07d1f67e.js";import"./index-b8d80492.js";import"./index-9b5f736f.js";import"./cn-ad0adaed.js";import"./WithLabel-fc65b61c.js";const j={component:D,parameters:{layout:"centered"},tags:["autodocs"]},a={},r={args:{disabled:!0}},e={args:{label:"Airplane Mode"}},s={args:{...r.args,...e.args}};var o,t,i;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:"{}",...(i=(t=a.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};var p,c,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  }
+}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var n,d,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    label: "Airplane Mode"
+  }
+}`,...(l=(d=e.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var u,g,b;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ...Disabled.args,
+    ...WithLabel.args
+  }
+}`,...(b=(g=s.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};const k=["Default","Disabled","WithLabel","WithLabelDisabled"];export{a as Default,r as Disabled,e as WithLabel,s as WithLabelDisabled,k as __namedExportsOrder,j as default};
+//# sourceMappingURL=Switch.stories-ec82729a.js.map
