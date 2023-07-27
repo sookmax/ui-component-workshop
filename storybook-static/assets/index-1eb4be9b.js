0fe6caefdc82f29ce7d3bc1a9f0719894e448aab
sook <sukkyu.chung@gmail.com>
2023-07-27 22:37:29 +0900

    build

---
+++ storybook-static/assets/index-1eb4be9b.js
@@ -0,0 +1,2 @@
+import{_ as a}from"./extends-98964cd2.js";import{r}from"./index-c4905b50.js";import{$ as o}from"./index-746ec687.js";const p=r.forwardRef((e,t)=>r.createElement(o.span,a({},e,{ref:t,style:{position:"absolute",border:0,width:1,height:1,padding:0,margin:-1,overflow:"hidden",clip:"rect(0, 0, 0, 0)",whiteSpace:"nowrap",wordWrap:"normal",...e.style}})));export{p as $};
+//# sourceMappingURL=index-1eb4be9b.js.map
