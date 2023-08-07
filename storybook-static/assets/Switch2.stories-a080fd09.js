d378cc0054374f589191fff7f37e402f595599e7
sook <sukkyu.chung@gmail.com>
2023-08-07 23:34:07 +0900

    build

---
+++ storybook-static/assets/Switch2.stories-a080fd09.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-3cfe299f.js";import{W as m}from"./WithLabel2-e26b90fd.js";import{r as c}from"./index-c4905b50.js";import{$ as d,a as f}from"./index-5ddb46ba.js";import{c as l}from"./cn-ad0adaed.js";import{L as u}from"./Label-f52c5603.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index-027e524b.js";import"./index-ad15057c.js";import"./index-746ec687.js";import"./index-07d1f67e.js";import"./index-b8d80492.js";import"./index-9b5f736f.js";const o=c.forwardRef(({className:e,...r},p)=>t.jsx(d,{className:l(`
+      peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center 
+      rounded-full border-2 border-transparent 
+      transition-colors 
+      focus-visible:outline-none 
+      focus-visible:ring-2 
+      focus-visible:ring-cyan-700
+      focus-visible:ring-offset-2 
+      focus-visible:ring-offset-white
+      disabled:cursor-not-allowed 
+      disabled:opacity-50 
+      data-[state=checked]:bg-cyan-700
+      data-[state=unchecked]:bg-gray-200
+      `,e),...r,ref:p,children:t.jsx(f,{className:`
+      pointer-events-none 
+      block h-5 w-5 rounded-full 
+      bg-white shadow-lg ring-0 
+      transition-transform 
+      data-[state=checked]:translate-x-5 
+      data-[state=unchecked]:translate-x-0
+      `})}));o.displayName=d.displayName;try{o.displayName="Switch",o.__docgenInfo={description:"",displayName:"Switch",props:{asChild:{defaultValue:null,description:"",name:"asChild",required:!1,type:{name:"boolean"}}}}}catch{}function b(){const e=`${c.useId()}-airplane-mode-switch`;return t.jsx(m,{labelPosition:"right",label:({className:r})=>t.jsx(u,{htmlFor:e,className:l(r,"cursor-pointer"),children:"Airplane Mode"}),target:({className:r})=>t.jsx(o,{id:e,className:r})})}const L={component:b,parameters:{layout:"centered"},tags:["autodocs"]},a={};var s,i,n;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(n=(i=a.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const C=["Default"];export{a as Default,C as __namedExportsOrder,L as default};
+//# sourceMappingURL=Switch2.stories-a080fd09.js.map
